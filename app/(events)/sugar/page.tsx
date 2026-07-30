d35ff39b7aa81c49477594efa301665e2147abc3
ikbal arslan <ikbalarslandev@gmail.com>
2026-07-30 19:11:25 +0300

    improve coffee page

--- app/(events)/sugar/page.tsx
+++ app/(events)/sugar/page.tsx
@@ -1,9 +1,39 @@
-import CallendyButton from "@/components/services/calendy";
+"use client";
+
+import Step from "@/components/event/stepCard";
+import {
+  HISTORY_DATA,
+  CHOOSE_DATA,
+  CULTURE_DATA,
+  SUGAR_TASTING_DATA,
+} from "@/data/sugar";
+
+import HeaderSection from "@/components/event/header";
+import CallendyBanner from "@/components/general/calendyBanner";
+import TastingBanner from "@/components/event/tasting";
 
 export default function SugarEventsPage() {
   return (
     <div>
-      <CallendyButton type="sugar" />
+      <HeaderSection
+        title="Sugar"
+        description="Explore the rich history of Sugar while tasting it"
+      />
+      <div className="flex flex-col gap-3 mx-2">
+        <Step
+          stepNumber={1}
+          title="History of Coffee"
+          data={HISTORY_DATA}
+          duration="1 hour"
+        />
+
+        <TastingBanner
+          stepNumber={4}
+          tastingData={SUGAR_TASTING_DATA}
+          duration="20 minutes"
+        />
+        <CallendyBanner type="sugar" />
+      </div>
     </div>
   );
 }
---
+++ data/sugar.ts
@@ -0,0 +1,46 @@
+const HISTORY_DATA = [
+  { label: "8000 BC", description: "Sugarcane Discovery" },
+  { label: "500 BC", description: "Invention of Sugar" },
+  { label: "Ancient Persia", description: "First Sugar Trade" },
+  { label: "7th Century", description: "White Sugar Invention" },
+  { label: "13th Century", description: "Ispah Rebellion" },
+  { label: "13th–14th Century", description: "Introduction to Europe" },
+  { label: "15th Century", description: "Madeira" },
+  { label: "1493", description: "Sugar Reaches the New World" },
+  { label: "1534", description: "Brazilian Sugar Colonies" },
+  { label: "16th Century", description: "Atlantic Slave Trade" },
+  { label: "17th Century", description: "Birth of Rum" },
+  { label: "17th Century", description: "Age of Pirates" },
+  { label: "1624", description: "Creating Taiwan" },
+  { label: "1811", description: "Sugar Beet Discovery" },
+  { label: "1850", description: "Sugar Consumption Boom" },
+  { label: "1930s–1940s", description: "Zyklon B" },
+];
+
+const CHOOSE_DATA = [
+  { label: "Extraction", description: "Seed processing methods" },
+  { label: "Altitude", description: "Flavor complexity" },
+  { label: "Roasting", description: "Light to dark profiles" },
+  { label: "Caffeine", description: "Energy levels" },
+  { label: "Brewing", description: "2 different techniques" },
+];
+
+const CULTURE_DATA = [
+  { label: "France", description: "Café au lait culture" },
+  { label: "Japan", description: "Siphon & pour-over" },
+  { label: "Turkey", description: "Cezve & fortune-telling" },
+  { label: "Mexico", description: "Café de olla" },
+];
+
+const SUGAR_TASTING_DATA = [
+  {
+    name: "Traditional Turkish Sugar",
+    notes: "from sugar beets",
+  },
+  {
+    name: "Classic Sugar Cubes",
+    notes: "from sugar cane",
+  },
+];
+
+export { HISTORY_DATA, CHOOSE_DATA, CULTURE_DATA, SUGAR_TASTING_DATA };

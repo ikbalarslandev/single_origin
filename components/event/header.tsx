9e6bddca816c9cbeed149494d379f45fb7dba36f
ikbal arslan <ikbalarslandev@gmail.com>
2026-07-30 16:27:00 +0300

    improve coffee page

--- components/event/header.tsx
+++ components/event/header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface HeaderSectionProps {
   title: string;
   description?: string;
@@ -11,6 +13,13 @@ const HeaderSection = ({
 }: HeaderSectionProps) => {
   return (
     <div className={`text-center mb-5 ${className}`}>
+      <Link
+        href="/"
+        className="absolute left-4 top-0-translate-y-1/2 text-blood-light text-3xl hover:opacity-70 transition-opacity"
+      >
+        &larr;
+      </Link>
+
       <h1 className="text-2xl font-bold text-blood-light mt-5">{title}</h1>
       {description && <p className="text-blood-dark mt-2">{description}</p>}
     </div>
